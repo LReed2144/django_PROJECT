2c3bf12d871bd0ebcd42441284e40b1387d99c96
Lyndsey Reed <43240450+LReed2144@users.noreply.github.com>
2022-09-22 22:35:44 -0700

    connecting a second app urls

--- hello/views.py
+++ hello/views.py
@@ -1,3 +1,10 @@
+#this imports the ability to do an HttpRequestion which is a special class in django.  in order to use it we must import it
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+#in order to create a view in django we must define a function
+def index(request):
+    return HttpResponse('Hello, World!')
+
