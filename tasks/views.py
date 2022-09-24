ba40265ade0d09b1010b6e54806b6ffe5b567561
Lyndsey Reed <43240450+LReed2144@users.noreply.github.com>
2022-09-23 17:58:39 -0700

    import reverse engineer for location

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,7 @@
 from django import forms
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 tasks = ["a", "b", "c"]
 
@@ -18,8 +20,9 @@ def add(request):
     if request.method == "POST":
         form = NewTaskForm(request.POST)
         if form.is_valid():
-            task = form.cleaned_data["tasks"]
+            task = form.cleaned_data["task"]
             tasks.append(task)
+            return HttpResponseRedirect(reverse(tasks:index))
         else:
             return render(request, "tasks/add.html", {
                 "form": form
